56b03e26035ec8ba572af306be9b780360e69b30
Wen (Alex) Chen <47618496+alexwenchen@users.noreply.github.com>
2022-11-07 17:11:03 -0600

    fixed navigation bar color bugging out

--- components/Nav.js
+++ components/Nav.js
@@ -8,7 +8,7 @@ const Nav = () => {
             <li>
                 <Link href='/'>
                   <button class="text-white text-xl focus:outline-none text-black bg-transparent hover:bg-green-400 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2">
-                    Introduction
+                    <text class="text-white">Introduction</text>
                   </button>
                 </Link>
             </li>
