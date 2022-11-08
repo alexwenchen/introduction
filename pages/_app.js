569cd3b3d40d76099a6530c0d27028334a6adcb8
Wen (Alex) Chen <47618496+alexwenchen@users.noreply.github.com>
2022-11-07 21:53:12 -0600

    added analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,16 @@
 import Layout from '../components/Layout'
 import '../styles/globals.css'
 
+import { Analytics } from '@vercel/analytics/react';
+
 function MyApp({ Component, pageProps }) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+      <Analytics />
+    </>
   )
 }
 
