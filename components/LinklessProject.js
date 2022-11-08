3e48c0667996a74e87d86212d332372ff8a30659
Wen (Alex) Chen <47618496+alexwenchen@users.noreply.github.com>
2022-11-07 18:32:51 -0600

    updated font size

--- components/LinklessProject.js
+++ components/LinklessProject.js
@@ -6,7 +6,7 @@ const Project = ({title, description}) => {
     return (
         <>
             <button class="text-2xl text-white bg-blue-900 hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-900 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 h-32 w-full">
-                {title} <br/>
+                <span class="text-2xl">{title}</span> <br/>
                 <span class="text-sm"> {description} </span>
             </button>
         </>
--- components/Project.js
+++ components/Project.js
@@ -7,7 +7,7 @@ const Project = ({title, link, description}) => {
         <>
             <Link href={link}>
                 <button class="text-2xl text-white bg-blue-800 hover:bg-blue-600 focus:ring-4 focus:outline-none focus:ring-green-300 dark:focus:ring-blue-900 shadow-lg shadow-blue-500/50 dark:shadow-lg dark:shadow-blue-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 h-32 w-full">
-                    {title} <br/>
+                    <span class="text-2xl">{title}</span> <br/>
                     <span class="text-sm"> {description} </span>
                 </button>
             </Link>
